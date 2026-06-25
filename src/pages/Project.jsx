04c6add83e633f4b06fdcf7ca63ef16ab3342f1a
Julian Ritou <julian.ritou@gmail.com>
2026-06-25 07:55:49 +0200

    test lien externe projet OC

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,8 +1,15 @@
 import { useParams } from "react-router-dom";
 
 function Project() {
-  const { id } = useParams();
-  return <h1>Fiche projet {id}</h1>;
+    const { id } = useParams();
+    return (
+        <div>
+            <h1>Fiche projet {id}</h1>
+            <a href='https://julianritou-collab.github.io/Nina-Carducci-Optimisation/' target="_blank" rel="noreferrer">
+                Voir le site du projet
+            </a>
+        </div>
+    );
 }
 
 export default Project;
