0d93db6b178a4410de3ed4c10ee2d55185d47428
Julian Ritou <julian.ritou@gmail.com>
2026-06-27 10:11:16 +0200

    page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <h1>404 - Page non trouvée</h1>;
+  return (
+    <div className="not-found">
+      <h1 className="not-found__code">404</h1>
+      <p className="not-found__message">Oups! La page que<span className="newline-mobile"> </span>vous demandez n'existe pas.</p>
+      <Link to="/" className="not-found__link">Retourner sur la page d'accueil</Link>
+    </div>
+  );
 }
 
 export default NotFound;
